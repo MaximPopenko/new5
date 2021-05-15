d62269e3d6455236f7353b19e4003996e1737d2b
maxim <mamaxim.popenkomx@gmail.com>
2021-05-15 10:10:48 +0300

    commit_4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi");
+
+        System.out.println("ewewewewe");
+
         System.out.println("Hi");
         System.out.println("Hi");
         System.out.println("Hi");

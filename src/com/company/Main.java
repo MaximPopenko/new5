8c5d3af1b7305929c151c47e0ca89b5a1df96344
maxim <mamaxim.popenkomx@gmail.com>
2021-05-15 10:54:36 +0300

    commit_7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,32 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi");
 
-        System.out.println("ewewewewe");
-        System.out.println("ewewewewe");
-        System.out.println("ewewewewe");
 
-        System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
+        System.out.println("NewBr");
 
-
-
-        System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
-
-
-
-
-        System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
 
 
     }

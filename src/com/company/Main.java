0841f5bc673ac45f17aa8af615a87df98722dd6b
maxim <mamaxim.popenkomx@gmail.com>
2021-05-15 09:49:20 +0300

    commit_3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,14 @@ public class Main {
         System.out.println("Hi");
         System.out.println("Hi");
         System.out.println("Hi");
+        System.out.println("Hi");
+
+
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+
 
     }
 }

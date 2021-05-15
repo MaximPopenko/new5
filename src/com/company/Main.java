9d5c7d8a3bbd68935fa6720dca0fbdaa7dbd7c34
maxim <mamaxim.popenkomx@gmail.com>
2021-05-15 11:09:17 +0300

    commit_8

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,51 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi");
 
+        System.out.println("ewewewewe");
+        System.out.println("ewewewewe");
+        System.out.println("ewewewewe");
 
-        System.out.println("NewBr");
-        System.out.println("NewBr");
-        System.out.println("NewBr");
-        System.out.println("NewBr");
-        System.out.println("NewBr");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+
+        System.out.println("ewewewewe");
+
+
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+
+
+
+
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
 
 
 
